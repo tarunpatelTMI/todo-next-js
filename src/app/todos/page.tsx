ce5572517ad37ffc51011c8db97dc95f2f4c8626
tarunpatelTMI <tarun.patel@trafficmanagement.com>
2024-01-02 22:39:05 +0530

    UI updates

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import{ ITodo } from "../../models/todo";
+import { ITodo } from "../../models/todo";
 import { useState, useEffect } from "react";
 import { useRouter } from "../../../node_modules/next/navigation";
 import { axiosInstance } from "../page";
@@ -50,7 +50,7 @@ const Todos = () => {
     router.push(`/todos/${todo.id}`)
   };
 
-  const handleCompleteTodo = async (todo:any) => {
+  const handleCompleteTodo = async (todo: any) => {
     const data = {
       desc: todo.desc,
       completed: !todo.completed,
@@ -60,26 +60,26 @@ const Todos = () => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-8 pt-8 bg-gray-100 pb-32">
-      <div className="text-2xl">Todo List Next</div>
+    <div className="flex h-screen flex-col items-center gap-8 pt-8 bg-gray-100 pb-32">
+      <div className="text-2xl">Your all todos</div>
       <div className="flex gap-2">
         <input
-          className="text-xl rounded shadow-md px-2"
+          className="text-l rounded shadow-md px-2"
           type="text"
           placeholder="Enter todo"
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
         />
         <button
-        disabled={inputText ==  ""}
-          className="text-xl shadow-md bg-blue-500 text-white hover:bg-blue-700 rounded-md px-2 py-1"
+          disabled={inputText == ""}
+          className="text-l shadow-md bg-blue-500 text-white hover:bg-blue-700 rounded-md px-2 py-1"
           onClick={handleSubmit}
         >
           Add
         </button>
         {todos && todos.length > 0 && (
           <button
-            className="text-xl shadow-md bg-red-300 text-white hover:bg-red-600 rounded-md px-2 py-1"
+            className="text-l shadow-md bg-red-300 text-white hover:bg-red-600 rounded-md px-2 py-1"
             onClick={handleDeleteAllTodos}
           >
             Delete All
@@ -90,20 +90,20 @@ const Todos = () => {
         {todos.map((item, index) => {
           return (
             <div className="bg-gray-100 flex justify-between items-center p-2 rounded-lg shadow-lg">
-              <div className="flex gap-2">
-                <input type="checkbox" className="" checked={item.completed} onClick={()=>handleCompleteTodo(item)} />
+              <div className="flex gap-2 items-center">
+                <input type="checkbox" className="h-5 w-5 self-center" checked={item.completed} onClick={() => handleCompleteTodo(item)} />
                 <div className="text-lg">{item.desc}</div>
               </div>
               <div className="flex gap-2">
                 <button
                   onClick={() => handleEditTodo(item)}
-                  className="text-xl shadow-md bg-green-500 text-white hover:bg-green-700 rounded-md px-2 py-1"
+                  className="text-l shadow-md bg-green-500 text-white hover:bg-green-700 rounded-md px-2 py-1"
                 >
                   Edit
                 </button>
                 <button
                   onClick={() => handleDeleteTodo(item)}
-                  className="text-xl shadow-md bg-red-500 text-white hover:bg-red-700 rounded-md px-2 py-1"
+                  className="text-l shadow-md bg-red-500 text-white hover:bg-red-700 rounded-md px-2 py-1"
                 >
                   Delete
                 </button>
